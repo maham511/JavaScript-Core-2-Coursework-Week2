462d4688e01c391844e56382b52d5f0fcf4ba2f8
maham511 <maha.malik511@gmail.com>
2021-07-26 19:20:56 +0100

    Ex D - created, appended ul, with empty li elements

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,16 @@
 function todoList(todos) {
   // Write your code here...
+  let contentDiv = document.getElementById('content'); //store div
+  let list = document.createElement('ul'); //create and store ul
+  contentDiv.appendChild(list);
+  list.innerText = 'Todo List' //renders Todo List
+
+  //loop for array
+  todos.forEach(element => {
+      let listItem = document.createElement('li'); //create, store li
+      list.appendChild(listItem); //appends li for each todo
+   //  list.innerText = element; //renders [object Object]
+  });
 }
 
 const todos = [
