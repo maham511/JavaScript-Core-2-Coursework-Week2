fe3eca1bf1a7c56b7e6dc8f99ac35d4d3557e1d6
maham511 <maha.malik511@gmail.com>
2021-07-26 18:59:53 +0100

    Cleaned up Exercise C

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,51 +1,35 @@
 function readingList(books) {
   // Write your code here...
-  //store div and append
 
-  //loop each book:
-  //1. create li
-  //2. create <p> with book title and author name
-  //3. append p to li
-  //4. create img tag, use bookcover url for src
-  //5. append img to li
-  //6. give li backgroundColor if green(true) or red(false)
-  //7. create ul, append to div
+  let listEl = document.createElement("ul");            //create and store ul element
+  let contentDiv = document.getElementById("content");  //store div 
+  contentDiv.appendChild(listEl); //ul added to div     //append ul to parent #content div
 
-  //7. create ul, append to div
-  let listEl = document.createElement('ul');
-  let contentDiv = document.getElementById('content');
-  contentDiv.appendChild(listEl);  //ul added to div
-
-
-//creates and appends right num of li, renders index values
-//   books.forEach((element, index) => {
-//     let listItem = document.createElement("li"); //creates and stores li element
-//     listItem.innerText = index; //adds array item string in relevant li
-//     listEl.appendChild(listItem); //appends li to ul
-// })
-
+  //Loop through each book object, create, append, and update elements
   books.forEach((element, index) => {
+    //Element creation
     let listItem = document.createElement("li"); //creates and stores li element
     let titlePara = document.createElement("p"); //create para el
-    listItem.appendChild(titlePara); //appends 1 para per li element
-    // titlePara.innerText = element.title; //renders correct titles in list
-    titlePara.innerText = `${element.title} - ${element.author}`; //renders correct titles plus author in list
+    let image = document.createElement("img"); //create img
+
+    //Elements appended to respective parent elements
     listEl.appendChild(listItem); //appends li to ul
-    
-    let image = document.createElement('img');
-    listItem.appendChild(image);
+    listItem.appendChild(titlePara); //appends 1 para per li element
+    listItem.appendChild(image); //appends 1 image per li element
+
+    //Elements contents/attributes updated
+    titlePara.innerText = `${element.title} - ${element.author}`; //renders correct titles plus author in list
     image.src = element.url;
     image.height = 400;
-    if(element.alreadyRead === true) {
-      listItem.style.backgroundColor = 'green'
-    }
-    else if (element.alreadyRead === false) {
-      listItem.style.backgroundColor = "red"
+    if (element.alreadyRead === true) {
+      listItem.style.backgroundColor = "green";
+    } else if (element.alreadyRead === false) {
+      listItem.style.backgroundColor = "red";
     }
   });
-
 }
 
+ //url keys added to loop through
 const books = [
   {
     title: "The Design of Everyday Things",
@@ -57,14 +41,14 @@ const books = [
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
-    url: 'https://images-na.ssl-images-amazon.com/images/I/71HMyqG6MRL.jpg'
+    url: "https://images-na.ssl-images-amazon.com/images/I/71HMyqG6MRL.jpg",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
     alreadyRead: true,
-    url: 'https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg'
-  }
+    url: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
+  },
 ];
 
 readingList(books);
