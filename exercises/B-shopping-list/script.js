c9bf31b8efcbac6abd71d3bfdb59bf36b22fda8f
maham511 <maha.malik511@gmail.com>
2021-07-26 14:05:03 +0100

    ex 2 done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,16 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+
+  //create ul elem, appended to content div
+  let list = document.createElement("ul"); //creates and stores ul element
+  let contentDiv = document.getElementById("content"); //stores div
+  contentDiv.appendChild(list); //appends list element to div
+
+  //loops through people array, create li tags for each array item, append to ul
+  arrayOfPeople.forEach(element => {
+    let listItem = document.createElement("li"); //creates and stores li element
+    listItem.innerText = element;  //adds array item string in relevant li
+    list.appendChild(listItem);    //appends li to ul
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
