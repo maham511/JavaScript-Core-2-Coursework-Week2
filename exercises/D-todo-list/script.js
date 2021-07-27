5cc4400a8a610a67a9a94540474c26c56f2a9259
maham511 <maha.malik511@gmail.com>
2021-07-27 12:00:09 +0100

    Ex D finished

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,16 +1,31 @@
 function todoList(todos) {
   // Write your code here...
-  let contentDiv = document.getElementById('content'); //store div
-  let list = document.createElement('ul'); //create and store ul
+  let contentDiv = document.getElementById("content"); //store div
+  let list = document.createElement("ul"); //create and store ul
   contentDiv.appendChild(list);
-  list.innerText = 'Todo List' //renders Todo List
+  list.innerText = "Todo List"; //renders Todo List
 
   //loop for array
-  todos.forEach(element => {
-      let listItem = document.createElement('li'); //create, store li
-      list.appendChild(listItem); //appends li for each todo
-   //  list.innerText = element; //renders [object Object]
+  todos.forEach((element, index) => {
+    let listItem = document.createElement("li"); //create, store li
+    list.appendChild(listItem); //appends li for each todo
+    listItem.innerText = element.todo; //renders correct object values in each li
+    listItem.style.textDecoration = "none";
+
+    let onClickStrikethrough = function () {
+      // listItem.style.color = "green";
+      if (
+        listItem.style.textDecoration === "none"
+      ) {
+        listItem.style.textDecoration = "line-through";
+      } else if (listItem.style.textDecoration === "line-through") {
+        listItem.style.textDecoration = "none";
+      }
+    };
+    //Adds strikethrough on li2 if clicked on it
+    listItem.addEventListener("click", onClickStrikethrough);
   });
+
 }
 
 const todos = [
